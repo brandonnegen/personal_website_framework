bfa37f73123900318fbe322f52e795e64b688e8c
brandonnegen <bnegen@gmail.com>
2015-08-18 08:53:28 -0500

    Adds GitHub API Repos

--- client/scripts/controllers/controller.js
+++ client/scripts/controllers/controller.js
@@ -6,6 +6,11 @@ myApp.controller('AboutMeController', ['$scope', function($scope){
     console.log("About Me Controller");
 }]);
 
-myApp.controller('ProjectsController', ['$scope', function($scope){
+myApp.controller('ProjectsController', ['$scope', '$http', function($scope, $http){
     console.log("Projects Controller");
+    $http.get("https://api.github.com/users/brandonnegen/repos")
+        .success(function (data) {
+            $scope.repos = data;
+            $scope.reposFound = data.length > 0;
+        });
 }]);
